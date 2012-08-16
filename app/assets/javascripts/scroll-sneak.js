1c2732d81811c33aa7dbda2b20a083072305c85c
Kuan Chen <immachink@gmail.com>
2012-08-16 01:07:53 -0700

    compressed js files

--- app/assets/javascripts/scroll-sneak.js
+++ app/assets/javascripts/scroll-sneak.js
@@ -8,38 +8,5 @@
  *
  * Date: Mon Mar 8 10:00:00 2010 -0500
  */
-var ScrollSneak = function(prefix, wait) {
-    // clean up arguments (allows prefix to be optional - a bit of overkill)
-    if (typeof(wait) == 'undefined' && prefix === true) prefix = null, wait = true;
-    prefix = (typeof(prefix) == 'string' ? prefix : window.location.host).split('_').join('');
-    var pre_name;
-
-    // scroll function, if window.name matches, then scroll to that position and clean up window.name
-    this.scroll = function() {
-        if (window.name.search('^'+prefix+'_(\\d+)_(\\d+)_') == 0) {
-            var name = window.name.split('_');
-            window.scrollTo(name[1], name[2]);
-            window.name = name.slice(3).join('_');
-        }
-    }
-    // if not wait, scroll immediately
-    if (!wait) this.scroll();
-
-    this.sneak = function() {
-	// prevent multiple clicks from getting stored on window.name
-	if (typeof(pre_name) == 'undefined') pre_name = window.name;
-
-	// get the scroll positions
-        var top = 0, left = 0;
-        if (typeof(window.pageYOffset) == 'number') { // netscape
-            top = window.pageYOffset, left = window.pageXOffset;
-        } else if (document.body && (document.body.scrollLeft || document.body.scrollTop)) { // dom
-            top = document.body.scrollTop, left = document.body.scrollLeft;
-        } else if (document.documentElement && (document.documentElement.scrollLeft || document.documentElement.scrollTop)) { // ie6
-            top = document.documentElement.scrollTop, left = document.documentElement.scrollLeft;
-        }
-	// store the scroll
-        if (top || left) window.name = prefix + '_' + left + '_' + top + '_' + pre_name;
-        return true;
-    }
+var ScrollSneak=function(a,b){if(typeof b=="undefined"&&a===true)a=null,b=true;a=(typeof a=="string"?a:window.location.host).split("_").join("");var c;this.scroll=function(){if(window.name.search("^"+a+"_(\\d+)_(\\d+)_")==0){var b=window.name.split("_");window.scrollTo(b[1],b[2]);window.name=b.slice(3).join("_")}};if(!b)this.scroll();this.sneak=function(){if(typeof c=="undefined")c=window.name;var b=0,d=0;if(typeof window.pageYOffset=="number"){b=window.pageYOffset,d=window.pageXOffset}else if(document.body&&(document.body.scrollLeft||document.body.scrollTop)){b=document.body.scrollTop,d=document.body.scrollLeft}else if(document.documentElement&&(document.documentElement.scrollLeft||document.documentElement.scrollTop)){b=document.documentElement.scrollTop,d=document.documentElement.scrollLeft}if(b||d)window.name=a+"_"+d+"_"+b+"_"+c;return true}}
 }
